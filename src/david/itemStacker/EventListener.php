45f50caddd0fd7203fd47dbd161b967d918dcb3d
BlueGamesNetwork <142659461+Terpz710@users.noreply.github.com>
2023-09-13 02:47:27 -0700

    Update EventListener.php
    Removed unnecessary comments for brevity.
    Simplified the if statement for checking if the entity is an instance of ItemEntity.
    Removed the check for $entity->getId() !== $e->getId() since we're already checking if they are different instances.
    Improved indentation and formatting for readability.

--- src/david/itemStacker/EventListener.php
+++ src/david/itemStacker/EventListener.php
@@ -11,35 +11,31 @@ class EventListener implements Listener {
     /** @var Loader */
     private $plugin;
 
-    /**
-     * EventListener constructor.
-     *
-     * @param Loader $plugin
-     */
     public function __construct(Loader $plugin) {
         $this->plugin = $plugin;
     }
 
-    /**
-     * @param EntitySpawnEvent $event
-     */
     public function onEntitySpawn(EntitySpawnEvent $event) {
         $entity = $event->getEntity();
-        if ($entity instanceof ItemEntity) {
-            $position = $entity->getPosition();
-            $world = $position->getWorld();
-            $entities = $world->getNearbyEntities($entity->getBoundingBox()->expandedCopy(5, 5, 5));
-            if (empty($entities)) {
-                return;
-            }
-            $originalItem = $entity->getItem();
-            foreach ($entities as $e) {
-                if ($e instanceof ItemEntity && $entity->getId() !== $e->getId()) {
-                    $item = $e->getItem();
-                    if ($item->getId() === $originalItem->getId()) {
-                        $e->flagForDespawn();
-                        $entity->getItem()->setCount($originalItem->getCount() + $item->getCount());
-                    }
+        if (!$entity instanceof ItemEntity) {
+            return;
+        }
+        
+        $position = $entity->getPosition();
+        $world = $position->getWorld();
+        $entities = $world->getNearbyEntities($entity->getBoundingBox()->expandedCopy(5, 5, 5));
+
+        if (empty($entities)) {
+            return;
+        }
+
+        $originalItem = $entity->getItem();
+        foreach ($entities as $e) {
+            if ($e instanceof ItemEntity && $entity !== $e) {
+                $item = $e->getItem();
+                if ($item->equals($originalItem)) {
+                    $e->flagForDespawn();
+                    $entity->getItem()->setCount($originalItem->getCount() + $item->getCount());
                 }
             }
         }
