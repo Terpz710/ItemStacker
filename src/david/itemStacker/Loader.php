055a6816e4f69ed4e20896a0624202b5c23e9348
WavyCraftNetwork <142659461+Terpz710@users.noreply.github.com>
2025-03-28 00:39:01 -0700

    Update Loader.php

--- src/david/itemStacker/Loader.php
+++ src/david/itemStacker/Loader.php
@@ -1,12 +1,18 @@
 <?php
 
+declare(strict
+
 namespace david\itemStacker;
 
 use pocketmine\plugin\PluginBase;
 
+use DaPigGuy\libPiggyUpdateChecker\libPiggyUpdateChecker;
+
 class Loader extends PluginBase {
 
-    public function onEnable(): void {
+    protected function onEnable() : void{
         $this->getServer()->getPluginManager()->registerEvents(new EventListener(), $this);
+
+        libPiggyUpdateChecker::init($this);
     }
 }
