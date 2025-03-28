79a8c87d18b6148c3e07d0e2a9a538f93fcb985f
WavyCraftNetwork <142659461+Terpz710@users.noreply.github.com>
2025-03-28 00:41:40 -0700

    Update EventListener.php

--- src/david/itemStacker/EventListener.php
+++ src/david/itemStacker/EventListener.php
@@ -1,29 +1,34 @@
 <?php
 
+declare(strict_types=1);
+
 namespace david\itemStacker;
 
-use pocketmine\entity\object\ItemEntity;
-use pocketmine\event\entity\EntitySpawnEvent;
 use pocketmine\event\Listener;
+use pocketmine\event\entity\EntitySpawnEvent;
+
+use pocketmine\entity\object\ItemEntity;
 
 class EventListener implements Listener {
 
-    public function __construct() {
-    }
-
-    public function onEntitySpawn(EntitySpawnEvent $event) {
+    public function onEntitySpawn(EntitySpawnEvent $event) : void{
         $entity = $event->getEntity();
+        
         if (!$entity instanceof ItemEntity) {
             return;
         }
+        
         $position = $entity->getPosition();
         $world = $position->getWorld();
         $entities = $world->getNearbyEntities($entity->getBoundingBox()->expandedCopy(5, 5, 5));
+        
         if (empty($entities)) {
             return;
         }
+        
         $originalItem = $entity->getItem();
         $totalItemCount = $originalItem->getCount();
+        
         foreach ($entities as $e) {
             if ($e instanceof ItemEntity && $entity !== $e) {
                 $itemE = $e->getItem();
@@ -34,6 +39,7 @@ class EventListener implements Listener {
                 }
             }
         }
+        
         if ($totalItemCount > 1) {
             $itemName = $originalItem->getName();
             $tag = "§7" . $itemName . " §7x§b" . $totalItemCount;
